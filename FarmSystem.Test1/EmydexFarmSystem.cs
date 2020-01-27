74eef82da004413bd1220e6f3341b2c79c165c67
James Lynch <jlynch@actionpoint.ie>
2020-01-27 11:58:38 +0000

    Initial changes checkout

--- FarmSystem.Test1/EmydexFarmSystem.cs
+++ FarmSystem.Test1/EmydexFarmSystem.cs
@@ -1,34 +1,81 @@
-﻿using System;
+﻿using FarmSystem.Test2;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FarmSystem.Test1
 {
     public class EmydexFarmSystem
     {
+        private List<Animal> Animals {get; set;}
+        private int counter;
+
+        public EmydexFarmSystem()
+        {
+            Animals = new List<Animal>();
+            counter = 1;
+        }
+
         //TEST 1
         public void Enter(object animal)
         {
             //TODO Modify the code so that we can display the type of animal (cow, sheep etc) 
             //Hold all the animals so it is available for future activities
-            Console.WriteLine("Animal has entered the Emydex farm");
+            Console.WriteLine($"{animal.GetType().Name} has entered the Emydex farm");
+
+            Animal newAnimal = (Animal)animal;
+            newAnimal._ordinal = counter++;
+            Animals.Add(newAnimal);
         }
-     
+
         //TEST 2
         public void MakeNoise()
         {
             //Test 2 : Modify this method to make the animals talk
-            Console.WriteLine("There are no animals in the farm");
+            foreach(var animal in Animals.OrderBy(a => a._ordinal))
+            {
+                animal.Talk();
+            }
         }
 
         //TEST 3
         public void MilkAnimals()
         {
-            Console.WriteLine("Cannot identify the farm animals which can be milked");
+            foreach (var animal in Animals.OrderBy(a => a._ordinal))
+            {
+                if (animal is IMilkableAnimal milkableAnimal)
+                {
+                    milkableAnimal.ProduceMilk();
+                }
+            }
+
+            //Console.WriteLine("Cannot identify the farm animals which can be milked");
         }
 
         //TEST 4
         public void ReleaseAllAnimals()
         {
-           Console.WriteLine("There are still animals in the farm, farm is not free");
+            CheckIfFarmIsEmpty();
+
+            foreach (var animal in Animals.OrderBy(a => a._ordinal))
+            {
+                Console.WriteLine($"{animal.GetType().Name} has left the farm");
+                Animals.Remove(animal);
+            }
+
+            CheckIfFarmIsEmpty();
+
+            //Console.WriteLine("There are still animals in the farm, farm is not free");
         }
+
+        private void CheckIfFarmIsEmpty()
+        {
+            if (!Animals.Any())
+            {
+                Console.WriteLine("Emydex Farm is now empty");
+            }
+        }
+
     }
 }
--- FarmSystem.Test1/Horse.cs
+++ FarmSystem.Test1/Horse.cs
@@ -2,35 +2,10 @@
 
 namespace FarmSystem.Test1
 {
-    public class Horse 
-    {
-        private string _id;
-        private int _noOfLegs;
+    public class Horse : Animal
+    {       
 
-        public string Id
-        {
-            get { return _id; }
-            set
-            {
-                _id = value;
-            }
-        }
-
-
-        public int NoOfLegs
-        {
-            get
-            {
-                return _noOfLegs;
-            }
-            set
-            {
-                _noOfLegs = 4;
-            }
-        }
-        
-
-        public void Talk()
+        public override void Talk()
         {
             Console.WriteLine("Horse says neigh!");
         }
